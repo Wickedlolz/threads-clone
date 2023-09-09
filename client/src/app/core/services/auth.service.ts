2fe5c1119e7cf1e43b80d1f433d9bff6744ad367
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-09-09 14:47:38 +0300

    import interface

--- client/src/app/core/services/auth.service.ts
+++ client/src/app/core/services/auth.service.ts
@@ -1,14 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-
-interface IUser {
-  _id: string;
-  email: string;
-  firstName: string;
-  lastName: string;
-  photoURL: string;
-}
+import { IUser } from '../interfaces/user';
 
 @Injectable({
   providedIn: 'root',
