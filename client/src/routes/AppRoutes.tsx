a093eb92f4f682492f9a933d3548b2e3c09d82f6
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-09-19 10:39:15 +0300

    add new route

--- client/src/routes/AppRoutes.tsx
+++ client/src/routes/AppRoutes.tsx
@@ -7,14 +7,24 @@ import {
 } from 'react-router-dom';
 
 import MainLayout from '../layouts/MainLayout';
-import UserHeader from '../components/UserHeader';
 import Spinner from '../components/Spinner';
+
 const Login = lazy(() => import('../pages/Login'));
+const User = lazy(() => import('../pages/User'));
+const Post = lazy(() => import('../pages/Post'));
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path='/' element={<MainLayout />}>
-            <Route path='/' element={<UserHeader />} />
+            <Route path='/user' element={<User />} />
+            <Route
+                path='/:userName/post/:postId'
+                element={
+                    <Suspense fallback={<Spinner />}>
+                        <Post />
+                    </Suspense>
+                }
+            />
             <Route
                 path='/login'
                 element={
