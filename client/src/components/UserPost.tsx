5a4f2b606340b338d5ed0f54db00dc7c04e46287
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-09-19 10:38:31 +0300

    add user post component

---
+++ client/src/components/UserPost.tsx
@@ -0,0 +1,83 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+// import { IThread } from '../interfaces/thread';
+
+import Actions from './Actions';
+import { BsThreeDots } from 'react-icons/bs';
+import VerifiedBadge from '../assets/verified_badge.svg';
+
+// type UserPostType = {
+//     post?: IThread;
+// };
+
+const UserPost = () => {
+    const [liked, setLiked] = useState<boolean>(false);
+
+    return (
+        <Link to='/viktordimitrov/post/1'>
+            <div className='flex gap-3 mb-4 py-4'>
+                <div className='flex flex-col items-center'>
+                    <img
+                        className='w-12 h-12 rounded-full'
+                        src='https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80'
+                        alt='user avatar'
+                    />
+                    <div className='w-[1px] h-full bg-gray-500 my-2'></div>
+                    <div className='relative w-full'>
+                        <img
+                            className='absolute top-0 left-4 w-6 h-6 rounded-full'
+                            src='https://bit.ly/kent-c-dodds'
+                            alt='another user profile'
+                        />
+                        <img
+                            className='absolute bottom-0 right-[-5px] w-6 h-6 rounded-full'
+                            src='https://bit.ly/ryan-florence'
+                            alt='another user profile'
+                        />
+                        <img
+                            className='absolute bottom-0 left-1 w-6 h-6 rounded-full'
+                            src='https://bit.ly/prosper-baba'
+                            alt='another user profile'
+                        />
+                    </div>
+                </div>
+                <div className='flex-1 flex-col gap-2'>
+                    <div className='flex justify-between w-full'>
+                        <div className='flex w-full items-center gap-1'>
+                            <p className='font-sm font-bold'>viktordimitrov</p>
+                            <img
+                                className='w-4 h-4 object-cover'
+                                src={VerifiedBadge}
+                                alt='verified badge'
+                                width={16}
+                                height={16}
+                            />
+                        </div>
+                        <div className='flex items-center gap-3'>
+                            <p className='font-sm text-gray-400'>1d</p>
+                            <BsThreeDots />
+                        </div>
+                    </div>
+                    <p className='font-sm'>This is my first post</p>
+                    <div className='rounded-md overflow-hidden border-[1px] border-gray-400'>
+                        <img
+                            className='w-full'
+                            src='https://raw.githubusercontent.com/Wickedlolz/bazaar/main/src/assets/bannerone.jpeg'
+                            alt='first post'
+                        />
+                    </div>
+                    <div className='flex gap-3 my-2'>
+                        <Actions liked={liked} setLiked={setLiked} />
+                    </div>
+                    <div className='flex items-center gap-2'>
+                        <p className='text-gray-500 font-sm'>200 replies</p>
+                        <div className='w-0.5 h-0.5 rounded-full bg-gray-500'></div>
+                        <p className='text-gray-500 font-sm'>200 likes</p>
+                    </div>
+                </div>
+            </div>
+        </Link>
+    );
+};
+
+export default UserPost;
