2017b8a62dcabb827efb880ffc21169cac42c343
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-09-20 13:37:49 +0300

    add functionality and validation

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,6 +1,46 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAppDispatch } from '../store';
+import { setUser } from '../store/reduces/authSlice';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { authService } from '../services';
+import { toast } from 'react-toastify';
+
+import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai';
+
+interface Inputs {
+    name: string;
+    username: string;
+    email: string;
+    password: string;
+    rePassword: string;
+}
 
 const SignUp = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<Inputs>();
+    const navigate = useNavigate();
+    const dispatch = useAppDispatch();
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [showPassword, setShowPassword] = useState<boolean>(false);
+
+    const handleSignUp: SubmitHandler<Inputs> = (data) => {
+        setIsLoading(true);
+        authService
+            .register(data)
+            .then((user) => {
+                dispatch(setUser(user));
+                navigate('/');
+            })
+            .catch((error) => {
+                toast.error(error.message);
+            })
+            .finally(() => setIsLoading(false));
+    };
+
     return (
         <article className='flex min-h-full flex-1 flex-col justify-center px-6 lg:px-8'>
             <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
@@ -9,25 +49,39 @@ const SignUp = () => {
                 </h2>
             </div>
 
-            <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
-                <form className='space-y-6'>
+            <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-sm'>
+                <form
+                    className='space-y-6'
+                    onSubmit={handleSubmit(handleSignUp)}
+                >
                     <div className='w-full flex flex-col sm:flex-row justify-between'>
                         <div>
                             <label
                                 htmlFor='name'
                                 className='block text-sm font-medium leading-6'
                             >
-                                Name <span className='text-red-500'>*</span>
+                                Full Name{' '}
+                                <span className='text-red-500'>*</span>
                             </label>
                             <div className='mt-2'>
                                 <input
                                     id='name'
+                                    {...register('name', {
+                                        required: {
+                                            value: true,
+                                            message: 'Name is required!',
+                                        },
+                                    })}
                                     name='name'
                                     type='text'
-                                    required
                                     className='block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                                 />
                             </div>
+                            {errors.name?.type === 'required' && (
+                                <span className='text-red-500 text-sm'>
+                                    {errors.name.message}
+                                </span>
+                            )}
                         </div>
 
                         <div>
@@ -40,12 +94,22 @@ const SignUp = () => {
                             <div className='mt-2'>
                                 <input
                                     id='username'
+                                    {...register('username', {
+                                        required: {
+                                            value: true,
+                                            message: 'Username is required!',
+                                        },
+                                    })}
                                     name='username'
                                     type='text'
-                                    required
                                     className='block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                                 />
                             </div>
+                            {errors.username?.type === 'required' && (
+                                <span className='text-red-500 text-sm'>
+                                    {errors.username.message}
+                                </span>
+                            )}
                         </div>
                     </div>
                     <div>
@@ -59,13 +123,33 @@ const SignUp = () => {
                         <div className='mt-2'>
                             <input
                                 id='email'
+                                {...register('email', {
+                                    required: {
+                                        value: true,
+                                        message: 'Email is required!',
+                                    },
+                                    pattern: {
+                                        value: /^[A-Za-z0-9]{2,}@[a-z]+\.[a-z]{2,3}$/,
+                                        message:
+                                            'Invalid email. Email format must be (example@yahoo.com)',
+                                    },
+                                })}
                                 name='email'
                                 type='email'
                                 autoComplete='email'
-                                required
                                 className='block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                             />
                         </div>
+                        {errors.email?.type === 'required' && (
+                            <span className='text-red-500 text-sm'>
+                                {errors.email.message}
+                            </span>
+                        )}
+                        {errors.email?.type === 'pattern' && (
+                            <span className='text-red-500 text-sm'>
+                                {errors.email.message}
+                            </span>
+                        )}
                     </div>
 
                     <div>
@@ -77,20 +161,53 @@ const SignUp = () => {
                                 Password <span className='text-red-500'>*</span>
                             </label>
                         </div>
-                        <div className='mt-2'>
+                        <div className='mt-2 flex gap-1'>
                             <input
                                 id='password'
+                                {...register('password', {
+                                    required: {
+                                        value: true,
+                                        message: 'Password is required!',
+                                    },
+                                    minLength: {
+                                        value: 8,
+                                        message:
+                                            'Password must be at least 8 characters long.',
+                                    },
+                                })}
                                 name='password'
-                                type='password'
+                                type={showPassword ? 'text' : 'password'}
                                 autoComplete='current-password'
-                                required
-                                className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                                className='block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                             />
+                            <button
+                                onClick={() =>
+                                    setShowPassword((state) => !state)
+                                }
+                                className='bg-white text-black border-0 outline-none py-1 px-2 rounded-md'
+                            >
+                                {showPassword ? (
+                                    <AiOutlineEyeInvisible />
+                                ) : (
+                                    <AiOutlineEye />
+                                )}
+                            </button>
                         </div>
+                        {errors.password?.type === 'required' && (
+                            <span className='text-red-500 text-sm'>
+                                {errors.password.message}
+                            </span>
+                        )}
+                        {errors.password?.type === 'minLength' && (
+                            <span className='text-red-500 text-sm'>
+                                {errors.password.message}
+                            </span>
+                        )}
                     </div>
 
                     <div>
                         <button
+                            disabled={isLoading}
                             type='submit'
                             className='flex w-full justify-center rounded-md bg-gray-700 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 duration-300'
                         >
@@ -100,7 +217,7 @@ const SignUp = () => {
                 </form>
 
                 <p className='mt-10 text-center text-sm text-gray-500'>
-                    Already have account?{' '}
+                    Already a user?{' '}
                     <Link
                         to='/login'
                         className='font-semibold leading-6 text-blue-400 hover:text-blue-500'
