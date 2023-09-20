c2c35152aeb1185ea338802493da4808d58a4ccc
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-09-20 09:39:05 +0300

    add cors

--- api/src/config/express.js
+++ api/src/config/express.js
@@ -1,10 +1,14 @@
 /* eslint-disable quotes */
 const express = require('express');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const authMiddleware = require('../middlewares/auth');
 
+const whitelist = ['http://localhost:5000', 'http://localhost:5173'];
+
 module.exports = (app) => {
     app.use(express.json());
+    app.use(cors({ origin: whitelist, credentials: true }));
     app.use(express.urlencoded({ extended: true }));
     app.use(cookieParser(process.env.COOKIE_SECRET));
     app.use(authMiddleware());
