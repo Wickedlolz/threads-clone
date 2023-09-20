af135977752c00537f806b944c82b7e22afee640
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-09-20 18:56:49 +0300

    add routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,17 @@
 import { useEffect } from 'react';
-import { useAppDispatch } from './store';
-import AppRoutes from './routes/AppRoutes';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from './store';
 import { getUser } from './store/reduces/authSlice';
 
+import User from './pages/User';
+import Post from './pages/Post';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import Home from './pages/Home';
+import MainLayout from './layouts/MainLayout';
+
 const App = () => {
+    const user = useAppSelector((state) => state.auth.user);
     const dispatch = useAppDispatch();
 
     useEffect(() => {
@@ -11,7 +19,26 @@ const App = () => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
-    return <AppRoutes />;
+    return (
+        <Routes>
+            <Route element={<MainLayout />}>
+                <Route
+                    path='/'
+                    element={user ? <Home /> : <Navigate to='/login' />}
+                />
+                <Route path='/user' element={<User />} />
+                <Route path='/:username/post/:postId' element={<Post />} />
+                <Route
+                    path='/login'
+                    element={!user ? <Login /> : <Navigate to='/' />}
+                />
+                <Route
+                    path='/signup'
+                    element={!user ? <SignUp /> : <Navigate to='/' />}
+                />
+            </Route>
+        </Routes>
+    );
 };
 
 export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,11 +6,14 @@ import { store } from './store/index.ts';
 
 import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <Provider store={store}>
-            <App />
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
         </Provider>
     </React.StrictMode>
 );
