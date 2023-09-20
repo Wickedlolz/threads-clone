96f43d25d500bed7f5d35e0439c51f41fec7e520
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-09-20 13:37:23 +0300

    add functionality and validation

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,6 +1,49 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { authService } from '../services';
+import { toast } from 'react-toastify';
+import { useAppDispatch } from '../store';
+import { setUser } from '../store/reduces/authSlice';
+
+interface Inputs {
+    email: string;
+    password: string;
+}
 
 const Login = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<Inputs>();
+    const navigate = useNavigate();
+    const dispatch = useAppDispatch();
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    /**
+     * Handles user login using the provided email and password.
+     *
+     * @param {Object} data - User input containing email and password.
+     * @param {string} data.email - The user's email for authentication.
+     * @param {string} data.password - The user's password for authentication.
+     */
+    const handleLogin: SubmitHandler<Inputs> = (data) => {
+        setIsLoading(true);
+        authService
+            .login(data.email, data.password)
+            .then((user) => {
+                dispatch(setUser(user));
+                navigate('/');
+            })
+            .catch((error) => {
+                toast.error(error.message);
+            })
+            .finally(() => {
+                setIsLoading(false);
+            });
+    };
+
     return (
         <article className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
             <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
@@ -10,7 +53,10 @@ const Login = () => {
             </div>
 
             <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
-                <form className='space-y-6'>
+                <form
+                    className='space-y-6'
+                    onSubmit={handleSubmit(handleLogin)}
+                >
                     <div>
                         <label
                             htmlFor='email'
@@ -22,13 +68,23 @@ const Login = () => {
                         <div className='mt-2'>
                             <input
                                 id='email'
+                                {...register('email', {
+                                    required: {
+                                        value: true,
+                                        message: 'Email is required!',
+                                    },
+                                })}
                                 name='email'
                                 type='email'
                                 autoComplete='email'
-                                required
                                 className='block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                             />
                         </div>
+                        {errors.email?.type === 'required' && (
+                            <span className='text-red-500 text-sm'>
+                                {errors.email.message}
+                            </span>
+                        )}
                     </div>
 
                     <div>
@@ -43,17 +99,28 @@ const Login = () => {
                         <div className='mt-2'>
                             <input
                                 id='password'
+                                {...register('password', {
+                                    required: {
+                                        value: true,
+                                        message: 'Password is required!',
+                                    },
+                                })}
                                 name='password'
                                 type='password'
                                 autoComplete='current-password'
-                                required
-                                className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                                className='block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                             />
                         </div>
+                        {errors.password?.type === 'required' && (
+                            <span className='text-red-500 text-sm'>
+                                {errors.password.message}
+                            </span>
+                        )}
                     </div>
 
                     <div>
                         <button
+                            disabled={isLoading}
                             type='submit'
                             className='flex w-full justify-center rounded-md bg-gray-700 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 duration-300'
                         >
