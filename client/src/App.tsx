9983bc9a8b8f914a0212c159177078588989d89e
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-09-20 11:53:53 +0300

    add when load app make request for user profile

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,17 @@
+import { useEffect } from 'react';
+import { useAppDispatch } from './store';
 import AppRoutes from './routes/AppRoutes';
+import { getUser } from './store/reduces/authSlice';
 
-function App() {
+const App = () => {
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        dispatch(getUser());
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
     return <AppRoutes />;
-}
+};
 
 export default App;
