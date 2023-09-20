047e4918ed157ed13d61005a28c12f03a8280c7d
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-09-20 19:50:13 +0300

    rename component

--- client/src/components/UserThread.tsx
+++ client/src/components/UserThread.tsx
@@ -1,25 +1,27 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-// import { IThread } from '../interfaces/thread';
+import { IThread } from '../interfaces/thread';
 
 import Actions from './Actions';
 import { BsThreeDots } from 'react-icons/bs';
 import VerifiedBadge from '../assets/verified_badge.svg';
+import moment from 'moment';
 
-// type UserPostType = {
-//     post?: IThread;
-// };
+type UserThreadType = {
+    thread: IThread;
+};
 
-const UserPost = () => {
+const UserThread = ({ thread }: UserThreadType) => {
     const [liked, setLiked] = useState<boolean>(false);
+    const passedTime = moment(thread.createdAt).fromNow();
 
     return (
-        <Link to='/viktordimitrov/post/1'>
+        <Link to={`/${thread.postedBy.username}/thread/${thread._id}`}>
             <div className='flex gap-3 mb-4 py-4'>
                 <div className='flex flex-col items-center'>
                     <img
                         className='w-12 h-12 rounded-full'
-                        src='https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80'
+                        src={thread.postedBy.photoURL}
                         alt='user avatar'
                     />
                     <div className='w-[1px] h-full bg-gray-500 my-2'></div>
@@ -44,7 +46,9 @@ const UserPost = () => {
                 <div className='flex-1 flex-col gap-2'>
                     <div className='flex justify-between w-full'>
                         <div className='flex w-full items-center gap-1'>
-                            <p className='font-sm font-bold'>viktordimitrov</p>
+                            <p className='font-sm font-bold'>
+                                {thread.postedBy.username}
+                            </p>
                             <img
                                 className='w-4 h-4 object-cover'
                                 src={VerifiedBadge}
@@ -53,12 +57,14 @@ const UserPost = () => {
                                 height={16}
                             />
                         </div>
-                        <div className='flex items-center gap-3'>
-                            <p className='font-sm text-gray-400'>1d</p>
+                        <div className='flex items-center justify-between gap-3'>
+                            <p className='font-sm text-xs text- text-gray-400'>
+                                {passedTime}
+                            </p>
                             <BsThreeDots />
                         </div>
                     </div>
-                    <p className='font-sm'>This is my first post</p>
+                    <p className='font-sm'>{thread.text}</p>
                     <div className='rounded-md overflow-hidden border-[1px] border-gray-400'>
                         <img
                             className='w-full'
@@ -70,9 +76,13 @@ const UserPost = () => {
                         <Actions liked={liked} setLiked={setLiked} />
                     </div>
                     <div className='flex items-center gap-2'>
-                        <p className='text-gray-500 font-sm'>200 replies</p>
+                        <p className='text-gray-500 font-sm'>
+                            {thread.replies.length} replies
+                        </p>
                         <div className='w-0.5 h-0.5 rounded-full bg-gray-500'></div>
-                        <p className='text-gray-500 font-sm'>200 likes</p>
+                        <p className='text-gray-500 font-sm'>
+                            {thread.likes.length} likes
+                        </p>
                     </div>
                 </div>
             </div>
@@ -80,4 +90,4 @@ const UserPost = () => {
     );
 };
 
-export default UserPost;
+export default UserThread;
