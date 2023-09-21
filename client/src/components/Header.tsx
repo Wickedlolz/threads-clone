a78c5d6630771cd31ab3bbce56e9113de037c598
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-09-21 12:27:45 +0300

    add doc string and show header if is logged

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -17,6 +17,13 @@ const Header = () => {
     const user = useAppSelector((state) => state.auth.user);
     const dispatch = useAppDispatch();
 
+    /**
+     * Logs the user out by invoking the authService's logout function.
+     * Upon successful logout, it dispatches actions to remove the user and clear threads,
+     * then navigates to the home page.
+     *
+     * @returns {void}
+     */
     const handleLogout = () => {
         authService
             .logout()
@@ -31,40 +38,35 @@ const Header = () => {
     };
 
     return (
-        <header className='flex justify-between px-3 sm:justify-evenly mt-6 mb-12 items-center'>
+        <>
             {user ? (
-                <Link to='/' className='text-white'>
-                    <AiFillHome className='text-white' size={28} />
-                </Link>
-            ) : (
-                <div></div>
-            )}
+                <header className='flex justify-between px-3 sm:justify-evenly mt-6 mb-12 items-center'>
+                    <Link to='/' className='text-white'>
+                        <AiFillHome className='text-white' size={28} />
+                    </Link>
 
-            <img
-                className='w-7 h-7 object-cover'
-                src={DarkLogo}
-                alt='threads logo'
-                width={36}
-                height={36}
-            />
-            {user ? (
-                <div className='flex items-center gap-4 text-white'>
-                    <Link to={`/profile/${user.username}`}>
-                        <RxAvatar size={28} />
-                    </Link>
-                    <Link to={`/chat`}>
-                        <BsFillChatQuoteFill size={24} />
-                    </Link>
-                    <button className='text-xs' onClick={handleLogout}>
-                        <FiLogOut size={24} />
-                    </button>
-                </div>
-            ) : (
-                <Link className='primaryBtn' to='/signup'>
-                    Sign up
-                </Link>
-            )}
-        </header>
+                    <img
+                        className='w-7 h-7 object-cover'
+                        src={DarkLogo}
+                        alt='threads logo'
+                        width={36}
+                        height={36}
+                    />
+
+                    <div className='flex items-center gap-4 text-white'>
+                        <Link to={`/profile/${user?.username}`}>
+                            <RxAvatar size={28} />
+                        </Link>
+                        <Link to={`/chat`}>
+                            <BsFillChatQuoteFill size={24} />
+                        </Link>
+                        <button className='text-xs' onClick={handleLogout}>
+                            <FiLogOut size={24} />
+                        </button>
+                    </div>
+                </header>
+            ) : null}
+        </>
     );
 };
 
