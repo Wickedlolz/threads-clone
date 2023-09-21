120d5929c12ab3bdf48af32b2011ccf227ff7cc4
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-09-21 07:59:04 +0300

    add dispatch to load user feed

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,39 +1,27 @@
-import { useEffect, useState } from 'react';
-import { threadService } from '../services';
-import { toast } from 'react-toastify';
-import { IThread } from '../interfaces/thread';
-
-import Spinner from '../components/Spinner';
+import { useEffect } from 'react';
 import UserThread from '../components/UserThread';
+import { useAppDispatch, useAppSelector } from '../store';
+import { loadFeed } from '../store/reduces/threadsSlice';
 
 const Home = () => {
-    const [threads, setThreds] = useState<IThread[]>([]);
-    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const feed = useAppSelector((state) => state.threads.feed);
+    const dispatch = useAppDispatch();
 
     useEffect(() => {
-        setIsLoading(true);
-        threadService
-            .getFeed()
-            .then((feeds) => {
-                setThreds(feeds);
-            })
-            .catch((error) => {
-                toast.error(error.message);
-            })
-            .finally(() => setIsLoading(false));
-    }, []);
+        if (feed === null) {
+            dispatch(loadFeed());
+        }
+    }, [dispatch, feed]);
 
     return (
         <section>
-            {!isLoading && threads.length === 0 && (
-                <h1>Follow some users to see the feed</h1>
+            {feed?.length === 0 && (
+                <h1 className='text-center'>
+                    Follow some users to see the feed
+                </h1>
             )}
-            {isLoading && (
-                <div className='flex justify-center'>
-                    <Spinner />
-                </div>
-            )}
-            {threads.map((thread) => (
+
+            {feed?.map((thread) => (
                 <UserThread key={thread._id} thread={thread} />
             ))}
         </section>
