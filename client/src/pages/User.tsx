17b850f2bd2394d83bdd54aa76720cb4c3cc6944
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-09-21 11:13:19 +0300

    add useEffect and select user from redux

--- client/src/pages/User.tsx
+++ client/src/pages/User.tsx
@@ -1,14 +1,23 @@
+import { useEffect } from 'react';
+import { useAppSelector } from '../store';
+
 import UserHeader from '../components/UserHeader';
 import UserThread from '../components/UserThread';
 
 const User = () => {
+    const user = useAppSelector((state) => state.auth.user);
+
+    useEffect(() => {
+        // chack if current user username is equal to username params and if is show this info if not
+        // make request to api and get user info
+    }, []);
     return (
         <div>
             <UserHeader />
+            {/* <UserThread />
             <UserThread />
             <UserThread />
-            <UserThread />
-            <UserThread />
+            <UserThread /> */}
         </div>
     );
 };
