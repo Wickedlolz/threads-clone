cd9159ba6a39601a44e9ec21bbb08c6823396f60
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-09-21 09:45:40 +0300

    add cloudinary package and configure it

--- api/src/config/express.js
+++ api/src/config/express.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const authMiddleware = require('../middlewares/auth');
+const { v2: cloudinary } = require('cloudinary');
 
 const whitelist = ['http://localhost:5000', 'http://localhost:5173'];
 
@@ -12,4 +13,9 @@ module.exports = (app) => {
     app.use(express.urlencoded({ extended: true }));
     app.use(cookieParser(process.env.COOKIE_SECRET));
     app.use(authMiddleware());
+    cloudinary.config({
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET,
+    });
 };
