530944cd68fcfd37f9370153ab1a7dcb95499ea3
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-09-22 20:04:07 +0300

    add following

--- client/src/components/UserHeader.tsx
+++ client/src/components/UserHeader.tsx
@@ -72,59 +72,59 @@ const UserHeader = ({ user }: UserHeaderProps) => {
     };
 
     return (
-        <article className='relative p-3 flex flex-col gap-5'>
-            <div className='flex justify-between my-3'>
-                <div className='flex flex-col gap-4'>
-                    <h2 className='text-3xl font-bold'>{user?.name}</h2>
+        <article className="relative p-3 flex flex-col gap-5">
+            <div className="flex justify-between my-3">
+                <div className="flex flex-col gap-4">
+                    <h2 className="text-3xl font-bold">{user?.name}</h2>
                     <p>
                         {user?.username}{' '}
-                        <span className='text-xs bg-gray-800 text-gray-400 p-1.5 rounded-2xl'>
+                        <span className="text-xs bg-gray-800 text-gray-400 p-1.5 rounded-2xl">
                             threads.net
                         </span>
                     </p>
                 </div>
                 <div>
                     <img
-                        className='w-28 h-28 object-cover rounded-full'
+                        className="w-28 h-28 object-cover rounded-full"
                         src={user?.photoURL}
-                        alt='user profile'
+                        alt="user profile"
                     />
                 </div>
             </div>
             <p>{user?.bio}</p>
             {user._id === authUser?._id ? (
-                <Link to='/profile/update'>
-                    <button className='primaryBtn'>Update Profile</button>
+                <Link to="/profile/update">
+                    <button className="primaryBtn">Update Profile</button>
                 </Link>
             ) : (
                 <button
                     disabled={isLoading}
-                    className='primaryBtn w-28'
+                    className="primaryBtn w-28"
                     onClick={handleFollowUnfollow}
                 >
                     {isLoading && <Spinner isSmall />}
                     {following ? 'Unfollow' : 'Follow'}
                 </button>
             )}
-            <div className='flex justify-between mr-5'>
-                <p className='text-gray-400 flex gap-2'>
+            <div className="flex justify-between mr-5">
+                <p className="text-gray-400 flex gap-2">
                     <span>{user?.followers.length} followers</span> &#8226;{' '}
-                    <Link to='#/'>instagram.com</Link>
+                    <span>{user?.following.length} following</span>
                 </p>
-                <div className='flex gap-3 items-center'>
-                    <p className='text-3xl'>
+                <div className="flex gap-3 items-center">
+                    <p className="text-3xl">
                         <AiOutlineInstagram />
                     </p>
                     <p
-                        className='text-2xl cursor-pointer'
+                        className="text-2xl cursor-pointer"
                         onClick={() => setOpenMenu((state) => !state)}
                     >
                         <CgMoreO />
                     </p>
                     {openMenu && (
-                        <div className='absolute bottom-0 right-3 p-1 bg-gray-600 rounded-md text-white'>
+                        <div className="absolute bottom-0 right-3 p-1 bg-gray-600 rounded-md text-white">
                             <button
-                                className='hover:bg-gray-500 p-2'
+                                className="hover:bg-gray-500 p-2"
                                 onClick={copyUrl}
                             >
                                 Copy Link
@@ -133,12 +133,12 @@ const UserHeader = ({ user }: UserHeaderProps) => {
                     )}
                 </div>
             </div>
-            <div className='w-full flex'>
-                <div className='flex-1 border-b-[1px] border-b-white text-center pb-3 cursor-pointer'>
-                    <p className='font-bold'>Threds</p>
+            <div className="w-full flex">
+                <div className="flex-1 border-b-[1px] border-b-white text-center pb-3 cursor-pointer">
+                    <p className="font-bold">Threds</p>
                 </div>
-                <div className='flex-1 border-b-[1px] border-b-gray-400 text-center pb-3 cursor-pointer'>
-                    <p className='font-bold text-gray-400'>Replies</p>
+                <div className="flex-1 border-b-[1px] border-b-gray-400 text-center pb-3 cursor-pointer">
+                    <p className="font-bold text-gray-400">Replies</p>
                 </div>
             </div>
         </article>
