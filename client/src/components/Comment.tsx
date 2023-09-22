e96dc9bfa1ff255e27ef1ac6600a874d5f0ba547
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-09-22 16:33:46 +0300

    start implementing comment functionality

--- client/src/components/Comment.tsx
+++ client/src/components/Comment.tsx
@@ -2,32 +2,41 @@ import { useState } from 'react';
 
 import { BsThreeDots } from 'react-icons/bs';
 import Actions from './Actions';
+import { IReplay } from '../interfaces/thread';
+import moment from 'moment';
 
-const Comment = () => {
+type CommentProps = {
+    reply: IReplay;
+};
+
+const Comment = ({ reply }: CommentProps) => {
     const [liked, setLiked] = useState<boolean>(false);
+    const passedTime = moment(reply?.createdAt).fromNow();
 
     return (
         <>
-            <div className='flex gap-4 py-2 my-2 w-full'>
+            <div className="flex gap-4 py-2 my-2 w-full">
                 <img
-                    className='w-9 h-9 rounded-full'
-                    src='https://bit.ly/kent-c-dodds'
-                    alt='user avatart'
+                    className="w-9 h-9 rounded-full"
+                    src={reply?.userProfilePic}
+                    alt={reply?.username}
                 />
-                <div className='flex flex-col gap-1 w-full '>
-                    <div className='flex justify-between w-full items-center'>
-                        <p className='text-sm font-bold'>Peter Parker</p>
-                        <div className='flex gap-2 items-center'>
-                            <p className='text-sm text-gray-400'>1d</p>
+                <div className="flex flex-col gap-1 w-full ">
+                    <div className="flex justify-between w-full items-center">
+                        <p className="text-sm font-bold">{reply?.username}</p>
+                        <div className="flex gap-2 items-center">
+                            <p className="text-sm text-gray-400">
+                                {passedTime}
+                            </p>
                             <BsThreeDots />
                         </div>
                     </div>
-                    <p>Hey this looks great!</p>
-                    <Actions liked={liked} setLiked={setLiked} />
-                    <p className='text-gray-500 text-sm'>2 likes</p>
+                    <p>{reply?.text}</p>
+                    {/* <Actions thread={{}} /> */}
+                    <p className="text-gray-500 text-sm">2 likes</p>
                 </div>
             </div>
-            <p className='w-full h-[1px] bg-gray-500'></p>
+            <p className="w-full h-[1px] bg-gray-500"></p>
         </>
     );
 };
