30a97f7c4659f66dd736ccc40aba477c9d4d9232
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-09-22 16:32:50 +0300

    add svg components and like functionality

--- client/src/components/Actions.tsx
+++ client/src/components/Actions.tsx
@@ -1,100 +1,74 @@
-import { Dispatch, SetStateAction, MouseEvent } from 'react';
+import { MouseEvent, useState } from 'react';
+import { IThread } from '../interfaces/thread';
+import { useAppDispatch, useAppSelector } from '../store';
+import { toast } from 'react-toastify';
+import { threadService } from '../services';
+
+import RepostSvg from './RepostSvg';
+import ShareSvg from './ShareSvg';
+import CommentSvg from './CommentSvg';
+import LikeSvg from './LikeSvg';
+import { updateThread } from '../store/reduces/threadsSlice';
 
 type ActionsProps = {
-    liked: boolean;
-    setLiked: Dispatch<SetStateAction<boolean>>;
+    thread: IThread | null;
 };
 
-const Actions = ({ liked, setLiked }: ActionsProps) => {
+const Actions = ({ thread }: ActionsProps) => {
+    const user = useAppSelector((state) => state.auth.user);
+    const dispatch = useAppDispatch();
+    const [threadData, setThreadData] = useState<IThread | null>(thread);
+    const liked: boolean = thread?.likes.includes(user?._id || '') || false;
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
     const handleLikeAndUnlike = (event: MouseEvent) => {
         event.preventDefault();
-        setLiked((state) => !state);
+
+        if (isLoading) {
+            return;
+        }
+
+        if (!user) {
+            toast.error(
+                'You must be logged in to like and unlike this thread.'
+            );
+        }
+
+        setIsLoading(true);
+
+        threadService
+            .likeUnlikeThreadById(thread!._id)
+            .then((data) => {
+                setThreadData(data);
+                dispatch(updateThread(data));
+            })
+            .catch((error) => toast.error(error.message))
+            .finally(() => setIsLoading(false));
     };
 
     return (
-        <div className='flex gap-3 my-2'>
-            <svg
-                className='cursor-pointer'
-                aria-label='Like'
-                color={liked ? 'rgb(237, 73, 86)' : ''}
-                fill={liked ? 'rgb(237, 73, 86)' : 'transparent'}
-                height='19'
-                role='img'
-                viewBox='0 0 24 22'
-                width='20'
-                onClick={handleLikeAndUnlike}
-            >
-                <path
-                    d='M1 7.66c0 4.575 3.899 9.086 9.987 12.934.338.203.74.406 1.013.406.283 0 .686-.203 1.013-.406C19.1 16.746 23 12.234 23 7.66 23 3.736 20.245 1 16.672 1 14.603 1 12.98 1.94 12 3.352 11.042 1.952 9.408 1 7.328 1 3.766 1 1 3.736 1 7.66Z'
-                    stroke='currentColor'
-                    strokeWidth='2'
-                ></path>
-            </svg>
-            <svg
-                className='cursor-pointer'
-                aria-label='Comment'
-                color=''
-                fill=''
-                height='20'
-                role='img'
-                viewBox='0 0 24 24'
-                width='20'
-                // onClick={onOpen}
-            >
-                <title>Comment</title>
-                <path
-                    d='M20.656 17.008a9.993 9.993 0 1 0-3.59 3.615L22 22Z'
-                    fill='none'
-                    stroke='currentColor'
-                    strokeLinejoin='round'
-                    strokeWidth='2'
-                ></path>
-            </svg>
-            <svg
-                className='cursor-pointer'
-                aria-label='Repost'
-                color='currentColor'
-                fill='currentColor'
-                height='20'
-                role='img'
-                viewBox='0 0 24 24'
-                width='20'
-            >
-                <title>Repost</title>
-                <path
-                    fill=''
-                    d='M19.998 9.497a1 1 0 0 0-1 1v4.228a3.274 3.274 0 0 1-3.27 3.27h-5.313l1.791-1.787a1 1 0 0 0-1.412-1.416L7.29 18.287a1.004 1.004 0 0 0-.294.707v.001c0 .023.012.042.013.065a.923.923 0 0 0 .281.643l3.502 3.504a1 1 0 0 0 1.414-1.414l-1.797-1.798h5.318a5.276 5.276 0 0 0 5.27-5.27v-4.228a1 1 0 0 0-1-1Zm-6.41-3.496-1.795 1.795a1 1 0 1 0 1.414 1.414l3.5-3.5a1.003 1.003 0 0 0 0-1.417l-3.5-3.5a1 1 0 0 0-1.414 1.414l1.794 1.794H8.27A5.277 5.277 0 0 0 3 9.271V13.5a1 1 0 0 0 2 0V9.271a3.275 3.275 0 0 1 3.271-3.27Z'
-                ></path>
-            </svg>
-            <svg
-                className='cursor-pointer'
-                aria-label='Share'
-                color=''
-                fill='rgb(243, 245, 247)'
-                height='20'
-                role='img'
-                viewBox='0 0 24 24'
-                width='20'
-            >
-                <title>Share</title>
-                <line
-                    fill='none'
-                    stroke='currentColor'
-                    strokeLinejoin='round'
-                    strokeWidth='2'
-                    x1='22'
-                    x2='9.218'
-                    y1='3'
-                    y2='10.083'
-                ></line>
-                <polygon
-                    fill='none'
-                    points='11.698 20.334 22 3.001 2 3.001 9.218 10.084 11.698 20.334'
-                    stroke='currentColor'
-                    strokeLinejoin='round'
-                    strokeWidth='2'
-                ></polygon>
-            </svg>
+        <div>
+            <div className="flex gap-3 my-2">
+                <LikeSvg
+                    liked={liked}
+                    handleLikeAndUnlike={handleLikeAndUnlike}
+                />
+                <CommentSvg
+                    replyTo={threadData?.postedBy.username}
+                    threadId={threadData?._id}
+                />
+                <RepostSvg />
+                <ShareSvg />
+            </div>
+            <div className="flex gap-2 items-center">
+                <p className="text-gray-500 font-sm">
+                    {thread?.replies.length} replies
+                </p>
+                <div className="w-0.5 h-0.5 rounded-full bg-gray-500"></div>
+                <p className="text-gray-500 font-sm">
+                    {thread?.likes.length} likes
+                </p>
+            </div>
         </div>
     );
 };
