8346fe750f2dfec3642005900aafaba749782c15
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-09-22 11:34:31 +0300

    add create and get by id

--- client/src/services/threadService.ts
+++ client/src/services/threadService.ts
@@ -4,6 +4,8 @@ import { IThread } from '../interfaces/thread';
 const endpoints = {
     feed: '/api/v1/threads/feed',
     userThreads: (username: string) => `/api/v1/threads/${username}`,
+    creatThread: '/api/v1/threads',
+    getThreadById: (threadId: string) => `/api/v1/threads/thread/${threadId}`,
 };
 
 export const getFeed = () => {
@@ -12,4 +14,12 @@ export const getFeed = () => {
 
 export const getUserThreads = (username: string) => {
     return requester.get<IThread[]>(endpoints.userThreads(username));
+};
+
+export const createThread = (text: string, img: string) => {
+    return requester.post<IThread>(endpoints.creatThread, { text, img });
+};
+
+export const getThreadById = (threadId: string) => {
+    return requester.get<IThread>(endpoints.getThreadById(threadId));
 };
