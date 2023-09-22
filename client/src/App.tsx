a11763ddfe5d52a987e9ccc995166c7a0844dc42
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-09-22 18:20:25 +0300

    load components lazy

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,20 @@
-import { useEffect } from 'react';
+import { useEffect, lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { useAppDispatch } from './store';
 import { getUser } from './store/reduces/authSlice';
 
-import User from './pages/User';
-import Thread from './pages/Thread';
-import Login from './pages/Login';
-import SignUp from './pages/SignUp';
 import Home from './pages/Home';
 import MainLayout from './layouts/MainLayout';
-import UpdateProfile from './pages/UpdateProfile';
 import AuthGuard from './components/AuthGuard';
 import GuestGuard from './components/GuestGuard';
-import NotFound from './pages/NotFound';
+import Spinner from './components/Spinner';
+
+const User = lazy(() => import('./pages/User'));
+const Thread = lazy(() => import('./pages/Thread'));
+const Login = lazy(() => import('./pages/Login'));
+const SignUp = lazy(() => import('./pages/SignUp'));
+const UpdateProfile = lazy(() => import('./pages/UpdateProfile'));
+const NotFound = lazy(() => import('./pages/NotFound'));
 
 const App = () => {
     const dispatch = useAppDispatch();
@@ -27,18 +29,57 @@ const App = () => {
             <Route element={<MainLayout />}>
                 <Route element={<AuthGuard />}>
                     <Route path="/" element={<Home />} />
-                    <Route path="/profile/update" element={<UpdateProfile />} />
+                    <Route
+                        path="/profile/update"
+                        element={
+                            <Suspense fallback={<Spinner />}>
+                                <UpdateProfile />
+                            </Suspense>
+                        }
+                    />
                 </Route>
                 <Route
                     path="/:username/thread/:threadId"
-                    element={<Thread />}
+                    element={
+                        <Suspense fallback={<Spinner />}>
+                            <Thread />
+                        </Suspense>
+                    }
                 />
                 <Route element={<GuestGuard />}>
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/signup" element={<SignUp />} />
+                    <Route
+                        path="/login"
+                        element={
+                            <Suspense fallback={<Spinner />}>
+                                <Login />
+                            </Suspense>
+                        }
+                    />
+                    <Route
+                        path="/signup"
+                        element={
+                            <Suspense fallback={<Spinner />}>
+                                <SignUp />
+                            </Suspense>
+                        }
+                    />
                 </Route>
-                <Route path="/profile/:username" element={<User />} />
-                <Route path="/*" element={<NotFound />} />
+                <Route
+                    path="/profile/:username"
+                    element={
+                        <Suspense fallback={<Spinner />}>
+                            <User />
+                        </Suspense>
+                    }
+                />
+                <Route
+                    path="/*"
+                    element={
+                        <Suspense fallback={<Spinner />}>
+                            <NotFound />
+                        </Suspense>
+                    }
+                />
             </Route>
         </Routes>
     );
