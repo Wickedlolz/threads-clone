0ffbb29066df9edd0b081b4c051e95bcce1a93c4
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-09-24 12:18:37 +0300

    add get messages and create conversation service

--- client/src/services/messageService.ts
+++ client/src/services/messageService.ts
@@ -1,10 +1,24 @@
 import { requester } from '.';
 import { IConversation } from '../interfaces/conversation';
+import { IMessage } from '../interfaces/message';
 
 const endpoints = {
     getConversations: '/api/v1/messages/conversations',
+    getMessagesById: (participantId: string) =>
+        `/api/v1/messages/${participantId}`,
+    createConversation: '/api/v1/messages/conversations',
 };
 
 export const loadConversations = () => {
     return requester.get<IConversation[]>(endpoints.getConversations);
+};
+
+export const getMessagesById = (participantId: string) => {
+    return requester.get<IMessage[]>(endpoints.getMessagesById(participantId));
+};
+
+export const createConversation = (participantId: string) => {
+    return requester.post<IConversation>(endpoints.createConversation, {
+        participantId,
+    });
 };
