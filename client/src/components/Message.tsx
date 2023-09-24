066c1fa6aa229185b77a6f94ad5f54e681e6d340
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-09-24 16:51:45 +0300

    change bg color of sender and add checks icon

--- client/src/components/Message.tsx
+++ client/src/components/Message.tsx
@@ -1,5 +1,7 @@
-import { IMessage } from '../interfaces/message';
 import { useAppSelector } from '../store';
+import { IMessage } from '../interfaces/message';
+
+import { BsCheck2All } from 'react-icons/bs';
 
 type MessageProps = {
     ownMessage: boolean;
@@ -17,9 +19,18 @@ const Message = ({ ownMessage, message }: MessageProps) => {
             {ownMessage ? (
                 <div className="flex gap-2 self-end">
                     {message.text && (
-                        <p className="max-w-sm bg-blue-400 p-1.5 rounded-lg">
-                            {message.text}
-                        </p>
+                        <div className="flex flex-col w-full bg-gray-500 rounded-lg">
+                            <p className="max-w-sm bg-gray-500 py-1 px-2 break-words rounded-lg">
+                                {message.text}
+                            </p>
+                            <div
+                                className={`self-end mr-2 font-bold ${
+                                    message.seen ? 'text-blue-500' : ''
+                                }`}
+                            >
+                                <BsCheck2All size={16} />
+                            </div>
+                        </div>
                     )}
 
                     <img
@@ -36,7 +47,7 @@ const Message = ({ ownMessage, message }: MessageProps) => {
                         alt={selectedConversation?.participants[0].name}
                     />
                     {message.text && (
-                        <p className="max-w-sm bg-blue-400 p-1.5 rounded-lg">
+                        <p className="max-w-sm bg-blue-400 py-1 px-2 break-words rounded-lg">
                             {message.text}
                         </p>
                     )}
