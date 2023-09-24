89b8a3844b6b853e89b76d9a568d645b692b1c1c
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-09-24 17:12:19 +0300

    add icons in conversation and add link to user profile

--- client/src/components/MessageContainer.tsx
+++ client/src/components/MessageContainer.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../store';
 import { useSocketContext } from '../contexts/SocketContext';
 import { addConversations } from '../store/reduces/conversationSlice';
@@ -9,6 +10,9 @@ import { IMessage } from '../interfaces/message';
 import VerifiedBadge from '../assets/verified_badge.svg';
 import Message from './Message';
 import MessageInput from './MessageInput';
+import { RiDeleteBin5Line } from 'react-icons/ri';
+import { FiPhoneCall } from 'react-icons/fi';
+import { BsPersonAdd } from 'react-icons/bs';
 
 const MessageContainer = () => {
     const { selectedConversation, conversations } = useAppSelector(
@@ -92,24 +96,35 @@ const MessageContainer = () => {
     useEffect(() => {
         messageEndRef.current?.scrollTo({
             top: messageEndRef.current.scrollHeight,
-            behavior: 'smooth',
         });
     }, [messages]);
 
     return (
         <div className="flex flex-[70] flex-col bg-gray-800 rounded-lg p-2">
-            <div className="flex w-full h-12 items-center gap-2">
-                <img
-                    className="w-7 h-7 rounded-full cursor-pointer object-cover"
-                    src={selectedConversation?.participants[0].photoURL}
-                    alt={selectedConversation?.participants[0].name}
-                />
-                {selectedConversation?.participants[0].username}{' '}
-                <img
-                    className="w-4 h-4"
-                    src={VerifiedBadge}
-                    alt="verified badge"
-                />
+            <div className="flex w-full h-12 items-center gap-2 justify-between">
+                <div className="flex gap-2 items-center">
+                    <Link
+                        className="flex gap-2 items-center"
+                        to={`/profile/${selectedConversation?.participants[0].username}`}
+                    >
+                        <img
+                            className="w-7 h-7 rounded-full cursor-pointer object-cover"
+                            src={selectedConversation?.participants[0].photoURL}
+                            alt={selectedConversation?.participants[0].name}
+                        />
+                        {selectedConversation?.participants[0].username}{' '}
+                    </Link>
+                    <img
+                        className="w-4 h-4"
+                        src={VerifiedBadge}
+                        alt="verified badge"
+                    />
+                </div>
+                <div className="mr-2 flex items-center gap-3">
+                    <BsPersonAdd className="cursor-pointer" />
+                    <FiPhoneCall className="cursor-pointer" />
+                    <RiDeleteBin5Line className="cursor-pointer" />
+                </div>
             </div>
             <p className="w-full h-[1px] bg-gray-500"></p>
             <div
