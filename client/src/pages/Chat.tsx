4c5cd0fe811cc794d825a37ce674b3534bb48468
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-09-24 12:18:14 +0300

    add search user and create conversation if dont have

--- client/src/pages/Chat.tsx
+++ client/src/pages/Chat.tsx
@@ -1,24 +1,30 @@
-import { useEffect } from 'react';
+import { FormEvent, useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../store';
-
 import {
+    addConversation,
     getConversations,
+    selectConversation,
     updateConversations,
 } from '../store/reduces/conversationSlice';
-import { toast } from 'react-toastify';
 import { useSocketContext } from '../contexts/SocketContext';
+import { toast } from 'react-toastify';
 
 import { BiSearch } from 'react-icons/bi';
 import { GiConversation } from 'react-icons/gi';
 import Conversation from '../components/Conversation';
 import MessageContainer from '../components/MessageContainer';
+import Spinner from '../components/Spinner';
+import { messageService, userService } from '../services';
 
 const Chat = () => {
     const dispatch = useAppDispatch();
+    const currentUser = useAppSelector((state) => state.auth.user);
     const { conversations, selectedConversation } = useAppSelector(
         (state) => state.conversations
     );
     const { socket, onlineUsers } = useSocketContext();
+    const [userText, setUserText] = useState<string>('');
+    const [isLoadingUser, setIsLoadingUser] = useState<boolean>(false);
 
     useEffect(() => {
         dispatch(getConversations())
@@ -32,6 +38,52 @@ const Chat = () => {
         });
     }, [socket, dispatch]);
 
+    const handleConversationSearch = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if (isLoadingUser || userText.length === 0) {
+            return;
+        }
+
+        setIsLoadingUser(true);
+
+        userService
+            .getUserByUsername(userText)
+            .then((user) => {
+                const messagingYourself = user._id === currentUser?._id;
+                if (messagingYourself) {
+                    toast.error('You cannot message yourself');
+                    return;
+                }
+
+                const conversationAlreadyExists = conversations?.find(
+                    (conversation) =>
+                        conversation.participants[0]._id === user._id
+                );
+
+                if (conversationAlreadyExists) {
+                    dispatch(selectConversation(conversationAlreadyExists));
+                    return;
+                }
+
+                messageService
+                    .createConversation(user._id)
+                    .then((createdConversation) => {
+                        dispatch(
+                            addConversation({
+                                ...createdConversation,
+                                participants: [user, currentUser],
+                            })
+                        );
+                        setUserText('');
+                    })
+                    .catch((error) => {
+                        throw error;
+                    });
+            })
+            .catch((error) => toast.error(error.message))
+            .finally(() => setIsLoadingUser(false));
+    };
+
     return (
         <section className="absolute left-[50%] w-full lg:w-[750px] p-0 translate-x-[-50%]">
             <div className="flex flex-col md:flex-row gap-4 mx-auto">
@@ -39,15 +91,23 @@ const Chat = () => {
                     <p className="font-bold text-gray-300">
                         Your Conversations
                     </p>
-                    <form>
+                    <form onSubmit={handleConversationSearch}>
                         <div className="flex items-center gap-2">
                             <input
                                 className="p-1 text-black rounded-lg"
                                 type="text"
                                 placeholder="Search for a user"
+                                value={userText}
+                                onChange={(event) =>
+                                    setUserText(event.target.value)
+                                }
                             />
-                            <button>
-                                <BiSearch size={22} />
+                            <button type="submit" disabled={isLoadingUser}>
+                                {isLoadingUser ? (
+                                    <Spinner isSmall />
+                                ) : (
+                                    <BiSearch size={22} />
+                                )}
                             </button>
                         </div>
                     </form>
