ee70487bb39e379ff95c9a2f473499ea0b188ec5
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-09-24 17:12:35 +0300

    add link to user profile

--- client/src/components/Message.tsx
+++ client/src/components/Message.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { useAppSelector } from '../store';
 import { IMessage } from '../interfaces/message';
 
@@ -25,7 +26,7 @@ const Message = ({ ownMessage, message }: MessageProps) => {
                             </p>
                             <div
                                 className={`self-end mr-2 font-bold ${
-                                    message.seen ? 'text-blue-500' : ''
+                                    message.seen ? 'text-blue-300' : ''
                                 }`}
                             >
                                 <BsCheck2All size={16} />
@@ -41,11 +42,15 @@ const Message = ({ ownMessage, message }: MessageProps) => {
                 </div>
             ) : (
                 <div className="flex gap-2">
-                    <img
-                        className="w-9 h-9 rounded-full cursor-pointer object-cover"
-                        src={selectedConversation?.participants[0].photoURL}
-                        alt={selectedConversation?.participants[0].name}
-                    />
+                    <Link
+                        to={`/profile/${selectedConversation?.participants[0].username}`}
+                    >
+                        <img
+                            className="w-9 h-9 rounded-full cursor-pointer object-cover"
+                            src={selectedConversation?.participants[0].photoURL}
+                            alt={selectedConversation?.participants[0].name}
+                        />
+                    </Link>
                     {message.text && (
                         <p className="max-w-sm bg-blue-400 py-1 px-2 break-words rounded-lg">
                             {message.text}
