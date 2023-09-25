97c78e89f6d55eb6fc79d4908c8264727b890048
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-09-25 16:13:26 +0300

    add message sended time

--- client/src/components/Message.tsx
+++ client/src/components/Message.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import { useAppSelector } from '../store';
+import moment from 'moment';
 import { IMessage } from '../interfaces/message';
 
 import { BsCheck2All } from 'react-icons/bs';
@@ -14,21 +15,25 @@ const Message = ({ ownMessage, message }: MessageProps) => {
         (state) => state.conversations.selectedConversation
     );
     const user = useAppSelector((state) => state.auth.user);
+    const createdAt = moment(message?.createdAt).fromNow();
 
     return (
-        <div className={ownMessage ? 'self-end' : ''}>
+        <div className={`${ownMessage ? 'self-end' : ''}`}>
             {ownMessage ? (
-                <div className="flex gap-2 self-end">
+                <div className="flex gap-2 self-end p-2">
                     {message.text && (
                         <div className="flex flex-col bg-gray-500 rounded-lg">
                             <p className="max-w-sm bg-gray-500 py-1 px-2 break-words rounded-lg">
                                 {message.text}
                             </p>
                             <div
-                                className={`self-end mr-2 font-bold ${
+                                className={`overflow-hidden flex items-center justify-end gap-1 self-end mr-2 font-bold ${
                                     message.seen ? 'text-blue-300' : ''
                                 }`}
                             >
+                                <span className="text-[9px] pl-1">
+                                    {createdAt}
+                                </span>
                                 <BsCheck2All size={16} />
                             </div>
                         </div>
