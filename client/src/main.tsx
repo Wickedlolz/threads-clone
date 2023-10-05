3ada4cb5faf02b9d3f0f452ccbeb04b361f89837
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-10-05 10:56:39 +0300

    improvements: separte routes in routes folder

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,14 +6,11 @@ import { store } from './store/index.ts';
 
 import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
+            <App />
         </Provider>
     </React.StrictMode>
 );
